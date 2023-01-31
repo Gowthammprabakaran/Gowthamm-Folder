03e5ff06e42d5afb065d4c8020d1baf7e93aaed2
gowthammprabakaran <gowthammprabakaran@gmail.com>
2023-01-31 21:12:41 +0530

    my task

---
+++ src/org/basic/FibonacciSeries.java
@@ -0,0 +1,34 @@
+package org.basic;
+
+public class FibonacciSeries {
+	
+	private void class1() {
+
+		System.out.println("earn");
+		
+	}
+
+	private void class2() {
+
+		System.out.println("vare");
+		
+	}
+
+	private void class3() {
+
+		System.out.println("gone");
+		
+	}
+	
+	public static void main(String[] args) {
+	FibonacciSeries fab = new FibonacciSeries();
+	
+	fab.class1();
+	fab.class2();
+	fab.class3();
+	
+		
+		
+	}
+
+}

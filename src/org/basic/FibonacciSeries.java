e33f16585db98caff52182c33caab1fc52fe8919
Gowthammprabakaran <gowthammprabakaran@gmail.com>
2023-02-12 17:42:43 +0530

    No Guts No Glory

--- src/org/basic/FibonacciSeries.java
+++ src/org/basic/FibonacciSeries.java
@@ -1,34 +1,37 @@
 package org.basic;
 
 public class FibonacciSeries {
-	
+
 	private void class1() {
 
-		System.out.println("earn");
-		
+		System.out.println("Ji");
+
 	}
 
 	private void class2() {
 
-		System.out.println("vare");
-		
+		System.out.println("Manakatha");
+
 	}
 
 	private void class3() {
 
-		System.out.println("gone");
-		
+		System.out.println("Billa");
+
 	}
-	
+
+	private void class5() {
+		System.out.println("DarkDevil");
+	}
+
 	public static void main(String[] args) {
-	FibonacciSeries fab = new FibonacciSeries();
-	
-	fab.class1();
-	fab.class2();
-	fab.class3();
-	
-		
-		
+		FibonacciSeries fab = new FibonacciSeries();
+
+		fab.class1();
+		fab.class2();
+		fab.class3();
+		fab.class5();
+
 	}
 
 }
